9a3856a13a9f6be5d7f84c89866bc6116ad9017b
hemal1020 <hemalahmed1020@gmail.com>
2026-08-05 14:38:29 +0600

    problem 88 solved

--- LeetCode/88.py
+++ LeetCode/88.py
@@ -1,18 +1,7 @@
-
-num = [[1,3],[2,2]]
-res = sorted(x for row in num for x in row)
-le = len(res)
-dup = None
-sum = 0
-n=0
-s =0
-for i in range(le):
-    if(i<le-1):
-        if (res[i] == res[i+1]):
-            dup = res[i]
-    n+=1        
-    sum = sum + n
-    s = s+ res[i]
-print(sum,s)    
-a = [dup, (sum-s)+dup]    
-print(a)
+import bisect as bs
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        temp = nums1[:m]
+        for i in range(n):
+            bs.insort_left(temp, nums2[i])
+        nums1[:]= temp
