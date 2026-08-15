66390f6f607d9a3c26e01c6d095cde4f2e289c45
hemal1020 <hemalahmed1020@gmail.com>
2026-08-15 14:33:35 +0600

    solved problem 1910

--- LeetCode/1910.py
+++ LeetCode/1910.py
@@ -1,2 +1,9 @@
-a = "acsaddcesfg"
-print(if "sad" in a )
+class Solution(object):
+    def removeOccurrences(self, s, part):
+        while(1):
+            if(part in s):
+                s = s.replace(part, "",1)
+            else:
+                break    
+        return s   
+                
