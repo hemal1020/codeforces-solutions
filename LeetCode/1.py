ab39bfc218c976355ddab9aa75bfddcfc8da413a
hemal1020 <hemalahmed1020@gmail.com>
2026-09-05 00:16:51 +0600

    new solution

---
+++ LeetCode/1.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        n ={}
+        for i in range(len(nums)):
+            first = nums[i]
+            second = target - first
+            if second in n:
+                return n[second],i
+            n[nums[i]]=i
+
+
+sol = Solution()
+print(sol.twoSum([0,2,9,8,5,3,15,78,7,4],7))
