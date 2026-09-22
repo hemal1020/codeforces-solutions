b6147c33430a57bb62b41a2f43adc007fea9f72f
hemal1020 <hemalahmed1020@gmail.com>
2026-09-23 00:15:56 +0600

    solved 935

---
+++ 935A.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+ans = 0
+
+for i in range(1, int(n ** 0.5) + 1):
+    if n % i == 0:
+        if i != n:
+            ans += 1
+        if n // i != i and n // i != n:
+            ans += 1
+
+print(ans)
