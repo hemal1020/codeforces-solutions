2ac8dcb939a5f2fb480c9098ab3370c036d3997b
hemal1020 <hemalahmed1020@gmail.com>
2026-09-23 00:26:41 +0600

    solved

---
+++ 2167B.py
@@ -0,0 +1,11 @@
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+
+    s, target = input().split()
+
+    if sorted(s) == sorted(target):
+        print("YES")
+    else:
+        print("NO")
